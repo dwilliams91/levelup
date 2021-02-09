8cec1b5c8e7bde8c52af8eba6b9a52ecad664faa
Your Github User Name <david.lav.williams@gmail.com>
2021-02-09 10:25:09 -0600

    renamed some data

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -3,6 +3,6 @@ from django.db import models
 class Event(models.Model):
 
     scheduler = models.ForeignKey("Gamer", on_delete=models.CASCADE)
-    game = models.ForeignKey("Games", on_delete=models.CASCADE)
+    game = models.ForeignKey("Game", on_delete=models.CASCADE)
     event_time=models.DateTimeField()
     location = models.CharField(max_length=50)
